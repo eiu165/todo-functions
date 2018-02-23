c39bab131dba7a684c1d6adb48c3274a0d863cbb
eiu165 <eiu165@gmail.com>
2018-02-23 06:42:34 -0600

    use small i for index.html

--- deployment/upload-www-to-blob.ps1
+++ deployment/upload-www-to-blob.ps1
@@ -1,7 +1,9 @@
 $ProgressPreference="SilentlyContinue"
 
 $destinationUri = "https://$($Env:STORAGE_HOSTNAME)/www"
+$destinationUri
 $destinationKey = $Env:STORAGE_KEY
+$destinationKey
 .\deployment\AzCopy\AzCopy.exe /Source:.\ToDoFunctions\www /Dest:$destinationUri /DestKey:$destinationKey /SetContentType /S /Y
 if ($LastExitCode -ne 0) { throw "azcopy returned code $($LastExitCode)" }
 
