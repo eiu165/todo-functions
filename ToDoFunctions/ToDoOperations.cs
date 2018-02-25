e7a0b146d7fc6706888c042c38d79f5cbb92d16d
eiu165 <eiu165@gmail.com>
2018-02-25 09:14:35 -0600

    format

--- ToDoFunctions/ToDoOperations.cs
+++ ToDoFunctions/ToDoOperations.cs
@@ -20,7 +20,10 @@ namespace ToDoFunctions
         public static ITelemetryClientFactory telemetryFactory = new TelemetryClientFactory();
 
         [FunctionName("GetAllToDos")]
-        public static HttpResponseMessage GetAllToDos([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "api/todos")]HttpRequestMessage req, [Table("todotable", Connection = "MyTable")]IQueryable<ToDoItem> inTable, TraceWriter log)
+        public static HttpResponseMessage GetAllToDos(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "api/todos")]HttpRequestMessage req, 
+            [Table("todotable", Connection = "MyTable")]IQueryable<ToDoItem> inTable, 
+            TraceWriter log)
         {
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
@@ -44,7 +47,11 @@ namespace ToDoFunctions
         }
 
         [FunctionName("GetToDo")]
-        public static HttpResponseMessage GetToDo([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "api/todos/{id}")]HttpRequestMessage req, [Table("todotable", Connection = "MyTable")]CloudTable table, string id, TraceWriter log)
+        public static HttpResponseMessage GetToDo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "api/todos/{id}")]HttpRequestMessage req, 
+            [Table("todotable", Connection = "MyTable")]CloudTable table, 
+            string id, 
+            TraceWriter log)
         {
             Stopwatch stopWatch = new Stopwatch();
             var item = table.GetToDoFromTable( id);
@@ -66,8 +73,13 @@ namespace ToDoFunctions
         }
 
         [FunctionName("CreateToDo")]
-        public static async Task<HttpResponseMessage> CreateToDo([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "api/todos")]HttpRequestMessage req, [Table("todotable", Connection = "MyTable")]CloudTable table, TraceWriter log, ExecutionContext context)
-        { TelemetryClient telemetryClient = telemetryFactory.GetClient();
+        public static async Task<HttpResponseMessage> CreateToDo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "api/todos")] HttpRequestMessage req, 
+            [Table("todotable", Connection = "MyTable")]CloudTable table, 
+            TraceWriter log, 
+            ExecutionContext context)
+        { 
+            TelemetryClient telemetryClient = telemetryFactory.GetClient();
             try
             {
                 Stopwatch stopWatch = new Stopwatch();
@@ -96,7 +108,11 @@ namespace ToDoFunctions
         }
 
         [FunctionName("UpdateToDo")]
-        public static async Task<HttpResponseMessage> UpdateToDo([HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "api/todos/{id}")]HttpRequestMessage req, [Table("todotable", Connection = "MyTable")]CloudTable table, string id, TraceWriter log)
+        public static async Task<HttpResponseMessage> UpdateToDo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "api/todos/{id}")]HttpRequestMessage req, 
+            [Table("todotable", Connection = "MyTable")]CloudTable table, 
+            string id, 
+            TraceWriter log)
         {
             Stopwatch stopWatch = new Stopwatch();
 
@@ -125,7 +141,11 @@ namespace ToDoFunctions
         }
 
         [FunctionName("SetCompleteToDo")]
-        public static async Task<HttpResponseMessage> SetCompleteToDo([HttpTrigger(AuthorizationLevel.Anonymous, "patch", Route = "api/todos/{id}")]HttpRequestMessage req, [Table("todotable", Connection = "MyTable")]CloudTable table, string id, TraceWriter log)
+        public static async Task<HttpResponseMessage> SetCompleteToDo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "patch", Route = "api/todos/{id}")]HttpRequestMessage req, 
+            [Table("todotable", Connection = "MyTable")]CloudTable table, 
+            string id, 
+            TraceWriter log)
         {
             Stopwatch stopWatch = new Stopwatch();
             var json = await req.Content.ReadAsStringAsync();
@@ -152,7 +172,10 @@ namespace ToDoFunctions
         }
 
         [FunctionName("DeleteToDo")]
-        public static HttpResponseMessage DeleteToDo([HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "api/todos/{id}")]HttpRequestMessage req, string id, [Table("todotable", Connection = "MyTable")]CloudTable table, TraceWriter log)
+        public static HttpResponseMessage DeleteToDo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "api/todos/{id}")]HttpRequestMessage req, 
+            string id, [Table("todotable", Connection = "MyTable")]CloudTable table, 
+            TraceWriter log)
         {
             Stopwatch stopWatch = new Stopwatch();
             table.DeleteToDoFromTable( id);
